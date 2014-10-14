286321d2cc736dbaa4c10f179954f12554977b52
Adam Hutchinson <ad.hutchinson@gmail.com>
2014-10-14 14:24:24 +0100

    vendor compiled assets

---
+++ public/assets/service_provided_line_items-3de36b72659b1a4cda8252e3e400d03a.js
@@ -0,0 +1,1 @@
+function update_amount(){document.getElementById("service_provided_line_item_amount").value=document.getElementById("service_provided_line_item_rate_per_person").value*document.getElementById("service_provided_line_item_no_people").value,document.getElementById("service_provided_line_item_amount").disabled=!1}
